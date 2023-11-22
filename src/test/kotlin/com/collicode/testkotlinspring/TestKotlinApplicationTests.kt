799de7f07de83d230bc7dc7b67039adbb8280f45
c-kiplimo <kiplimocollins855@gmail.com>
2023-11-22 13:07:41 +0300

    initial commit

---
+++ src/test/kotlin/com/collicode/testkotlinspring/TestKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.collicode.testkotlinspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
